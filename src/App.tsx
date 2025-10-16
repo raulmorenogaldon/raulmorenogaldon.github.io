10cb46474819ff36ff985b0367289678a9108f46
Raúl <raulmorenogaldon@gmail.com>
2025-10-16 13:04:52 +0200

    Fixed scrolling reset when changing page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import ScrollToTop from './components/ScrollToTop'
 import './App.css'
 
 const App = () => {
   return (
     <div className="app-shell">
+      <ScrollToTop />
       <Navbar />
       <main className="app-shell__main">
         <Outlet />
